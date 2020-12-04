4217b2e96710d954fc8b41ae6268897da25c6fb5
nickverse <nikoskalomiris@gmail.com>
2020-12-04 23:02:16 +0200

    Update kernel built script with lineage config

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,5 +4,5 @@ export ARCH=arm64
 export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=q
 
-make exynos7885-a20e_defconfig
+make lineage-17.1_defconfig
 make -j64
